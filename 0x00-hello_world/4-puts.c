c4d772ffb97401a3af3d325e4343e6e82120d36b
evie-8 <neouma29@gmail.com>
2023-02-16 14:48:55 +0300

    Using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts - returns string values
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char string[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(string);
+	return (0);
+}
