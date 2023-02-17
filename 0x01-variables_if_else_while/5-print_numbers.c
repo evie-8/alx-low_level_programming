8d650dd5e783d3b3a41c64b64ce4083c0a04bc55
evie-8 <neouma29@gmail.com>
2023-02-17 14:06:02 +0300

    numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - start of code
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+	printf("%d", i);
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
