db799d356af60ca4f47b0aaedfdc872f2f21b7db
evie-8 <neouma29@gmail.com>
2023-02-17 14:12:05 +0300

    all numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+	putchar(number);
+	number++;
+	}
+	putchar('\n');
+	return (0);
+}
