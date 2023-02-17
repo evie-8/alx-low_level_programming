aa222f0f7d1f58a9a86710afefc70f8415d33fa8
evie-8 <neouma29@gmail.com>
2023-02-17 13:10:50 +0300

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Starting point of code
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int letter = 97;
+
+	while (letter <= 122)
+	{
+	putchar(letter);
+	letter++;
+	}
+	return (0);
+}
