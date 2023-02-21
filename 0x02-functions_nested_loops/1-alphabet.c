4bc227404e1560f5f32adbeef4f768707c3d060b
evie-8 <neouma29@gmail.com>
2023-02-21 15:58:23 +0300

    lower case alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,9 @@
 /**
  * print_alphabet - print all letters in lower case
- * _putchar - output a character
- * @c: parameter to which argument in passed
- *Return: depending on the return of the actual function
  */
-int _putchar(char c);
 void print_alphabet(void)
 {
+	int _putchar(char c);
 	int letter = 97;
 
 	while (letter <= 122)
