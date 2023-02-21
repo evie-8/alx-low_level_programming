d52419567304a8dc3a159974758746006790a65b
evie-8 <neouma29@gmail.com>
2023-02-21 17:55:56 +0300

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of an integer
+ *
+ * @d: value to be passed
+ * Return: absolute value
+ */
+int _abs(int d)
+{
+	return ((d < 0) ? (d * -1) : d);
+}
