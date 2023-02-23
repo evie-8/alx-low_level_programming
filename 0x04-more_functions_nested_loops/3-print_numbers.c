deb48232889daf9e4721971b077589afb50bd7b5
evie-8 <neouma29@gmail.com>
2023-02-23 12:45:24 +0300

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		_putchar(number);
+		number++;
+	}
+	_putchar('\n');
+}
