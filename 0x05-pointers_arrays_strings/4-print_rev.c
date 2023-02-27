19ec3cca4d61164febe05e8f4f020c947af8d7e8
evie-8 <neouma29@gmail.com>
2023-02-27 15:27:50 +0300

    reversing a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - reverse string
+ * @s: string
+*/
+void print_rev(char *s)
+{
+	int c = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
+
+	for (i = c; i >= 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
