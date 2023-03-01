67c2a314669194557207a1a956b8401c080b0b32
evie-8 <neouma29@gmail.com>
2023-03-02 00:12:28 +0300

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,19 +7,9 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int value = 0;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			if (s1[i] < s2[i])
-				value = -15;
-			else if (s1[i] > s2[i])
-				value = 15;
-		break;
-		}
-	}
-	return value;
+	while ((s1[i] == s2[i]) && (s1[i] != '\0' || s2[i] != '\0'))
+		i++;
+	return (s1[i] - s2[i]);
 }
