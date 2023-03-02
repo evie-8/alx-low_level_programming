0801d375e68fa029c2a57d9fb499ca8c2eebb6df
evie-8 <neouma29@gmail.com>
2023-03-02 20:32:02 +0300

    integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+ #include "main.h"
+/**
+ * print_number - displays a number
+ * @n: number to be printed
+ */
+void print_number(int n)
+{
+	unsigned int number = n;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		number = n;
+		_putchar('-');
+	}
+	number = number / 10;
+	if (number != 0)
+		print_number(number);
+	_putchar((unsigned int)n % 10 + '0');
+}
