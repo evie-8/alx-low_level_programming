5dab4e5a491a80e4b792d29990fe2a53f8f110bd
evie-8 <neouma29@gmail.com>
2023-03-02 17:16:49 +0300

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,27 @@
 #include "main.h"
+/**
+ * cap_string - capitalize first letter of a word
+ * @s: string
+ * Return: modified string
+ */
 char *cap_string(char *s)
 {
-    int i;
-    char prev = ' ';
+	int i;
+	char prev = ' ';
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))
-        {
-            if (prev == ' ' || prev == '\t' || prev == '\n' ||
-                prev == ',' || prev == ';' || prev == '.' ||
-                prev == '!' || prev == '?' || prev == '"' ||
-                prev == '(' || prev == ')' || prev == '{' || prev == '}')
-            {
-                s[i] -= 32;
-            }
-        }
-        prev = s[i];
-    }
-    return s;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))
+		{
+			if (prev == ' ' || prev == '\t' || prev == '\n' ||
+			prev == ',' || prev == ';' || prev == '.' ||
+			prev == '!' || prev == '?' || prev == '"' ||
+			prev == '(' || prev == ')' || prev == '{' || prev == '}')
+			{
+				s[i] -= 32;
+			}
+		}
+		prev = s[i];
+	}
+	return (s);
 }
-
