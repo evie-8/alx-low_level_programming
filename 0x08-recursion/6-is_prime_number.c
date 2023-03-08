70c16810810d33ddd26b2d1ce2185ab60264168c
evie-8 <neouma29@gmail.com>
2023-03-08 11:08:15 +0300

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * is_prime_number - checks if number is prime
+ * @n: number to be checked
+ * Return: 1 if prime and 0 if not prime
+ */
+int is_prime_number(int n)
+{
+	int i, count = 0;
+
+	if (n <= 0)
+		return (0);
+	else if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if (n % i == 0)
+				count++;
+		}
+		if (count == 2)
+			return (1);
+	}
+	return (0);
+}
