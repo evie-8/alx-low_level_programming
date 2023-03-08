89f83c9b5d23546f48b12f5f11b7c785b3539374
evie-8 <neouma29@gmail.com>
2023-03-08 11:23:07 +0300

    u

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,15 +8,16 @@ int is_prime_number(int n)
 {
 	int i, count = 0;
 
-	if (n > 0)
+	if (n <= 1)
+		return (0);
+
+	for (i = 2; i <= n / 2; i++)
 	{
-		for (i = 1; i <= n; i++)
-		{
-			if (n % i == 0)
-				count++;
-		}
-		if (count == 2)
-			return (1);
+		if (n % i == 0)
+			count++;
 	}
-	return (0);
+	if (count == 0)
+		return (1);
+	else
+		return (0);
 }
