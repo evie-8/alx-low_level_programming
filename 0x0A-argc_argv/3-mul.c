e2b63952353e76b74b50401f2d1f259b7773a5f5
evie-8 <neouma29@gmail.com>
2023-03-10 15:19:31 +0300

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,13 @@ int main(int argc, char *argv[])
 {
 	int i, product = 1;
 
-	if (argc <= 1)
+	if (argc < 2)
 		printf("Error\n");
 	else
 	{
 	for (i = 1; i < argc; i++)
 	{
-		product *= atoi(argv[i]);
+			product *= atoi(argv[i]);
 	}
 	printf("%d\n", product);
 	}
