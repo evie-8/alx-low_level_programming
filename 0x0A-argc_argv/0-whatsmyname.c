74293b03eab2dfc6c7f970c8ff1541e79c2ae278
evie-8 <neouma29@gmail.com>
2023-03-10 14:38:10 +0300

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: counts number of arguments from command line
+ * @argv: a string that stores the arguments passed from command line
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
