fe482f0e70067871c129611b5f7c93faa89ff1cb
evie-8 <neouma29@gmail.com>
2023-03-16 15:43:11 +0300

    work

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * _calloc - allocates memory and each bit is intialized to zero
  * @nmemb: number of elements
@@ -9,16 +10,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *c;
-	unsigned int i;
-	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	c = malloc(size * nmemb);
 	if (c == NULL)
 		return (NULL);
-	p = (char *)c;
-	for (i = 0; i < nmemb; i++)
-		*(p + i) = 0;
+	memset(c, 0, nmemb * size);
 	return (c);
 }
