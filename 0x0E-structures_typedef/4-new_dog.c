ea54d0891f0eaad72560c85232dc04fed4fd0aa9
evie-8 <neouma29@gmail.com>
2023-03-21 00:29:16 +0300

    new instance for struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - new object of type dog
  * @name: name fo dog
@@ -9,13 +10,26 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new, *p;
+	dog_t *p;
 
-	p = &new;
+	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
-	new.name = name;
-	new.age = age;
-	new.owner = owner;
+	(*p).name = malloc(sizeof(char) * (strlen(name) + 1));
+	if ((*p).name == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	(*p).owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if ((*p).owner == NULL)
+	{
+		free((*p).name);
+		free(p);
+		return (NULL);
+	}
+	strcpy((*p).name, name);
+	strcpy((*p).owner, owner);
+	(*p).age = age;
 	return (p);
 }
