0dd25af6f2ee915d29dfd53d4d14d279814b1801
evie-8 <neouma29@gmail.com>
2023-03-20 20:18:59 +0300

    header file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# Using structures and typedef
+- Structures: these are user defined datatypes that take on any data type.
+- typedef: this is a keyword that can help you change the name of an existing variable
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef D0G_H_
+#define DOG_H_
+
+/**
+ * struct dog - attributes for dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
