05ac4b0de0ebc39f65bc92478f38890b6907183d
evie-8 <neouma29@gmail.com>
2023-03-23 05:56:47 +0300

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point. Perform simple operations on two numbers.
+ *
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
