194767583f46a91a9919adceb56570a416dde834
evie-8 <neouma29@gmail.com>
2023-04-07 22:01:02 +0300

    adding files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _is_binary - checks string
+ * @s: string
+ * Return: array
+ */
+unsigned int *_is_binary(const char *s)
+{
+	int j, i = 0;
+	unsigned int *arr;
+
+	i = strlen(s);
+	arr = malloc(sizeof(unsigned int) * i);
+	if (arr == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] == '1' || s[j] == '0')
+		{
+			if (s[j] == '1')
+				arr[j] = 1;
+			else
+				arr[j] = 0;
+
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	return (arr);
+}
+/**
+ * _pow - power of x
+ * @x: vase
+ * @y: power
+ * Return: int
+ */
+int _pow(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	return (x * _pow(x, y - 1));
+}
+/**
+ * binary_to_uint - from binary to unsigned int
+ * @b: string
+ * Return: int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int *num, sum = 0;
+	int y;
+
+	if (b == NULL)
+		return (0);
+
+	num = malloc(sizeof(unsigned int) * strlen(b));
+	num = _is_binary(b);
+	if (num == NULL)
+		return (0);
+
+	for (y = strlen(b) - 1; y >= 0; y--)
+	{
+		sum += num[strlen(b) - y - 1] * (_pow(2, y));
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,7 @@
+# Bitwise Manipulation.
+- We use the following operations:
+  - <<: left shift
+  - >>: right shift
+  - ^: XOR
+  - &: AND
+  - |: OR
