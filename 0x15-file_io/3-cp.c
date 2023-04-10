79f18ec3d9574bf006105aab02735ee2b108d048
evie-8 <neouma29@gmail.com>
2023-04-10 14:57:13 +0300

    more corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,37 +3,39 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
+#define buffer 1024
+
 /**
  * reads - reads file
  * @filename: name of file
- * @b: size
  * Return: string
  */
-char *reads(char *filename, size_t b)
+char *reads(char *filename)
 {
 	int x, y, z;
 	char *c;
 
-	c = malloc(sizeof(char) * b);
+	c = malloc(sizeof(char) * buffer);
 	x = open(filename, O_RDONLY);
 	if (x == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
-	y = read(x, c, b);
+	y = read(x, c, buffer);
 	if (y == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		free(c);
-		z = close(x);
+		close(x);
 		exit(98);
 	}
 	c[y] = '\0';
 	z = close(x);
 	if (z == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", x);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", x);
 		exit(100);
 	}
 	return (c);
@@ -49,32 +51,32 @@ int main(int argc, char *argv[])
 	int z, a, b;
 	char *c;
 
-	c = malloc(sizeof(char) * 1024);
+	c = malloc(sizeof(char) * buffer);
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	c = reads(argv[1], 1024);
+	c = reads(argv[1]);
 	umask(0);
 	z = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (z == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	a = write(z, c, strlen(c));
 	if (a == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(c);
-		b = close(z);
+		close(z);
 		exit(99);
 	}
 	b = close(z);
 	if (b  == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", z);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", z);
 		exit(100);
 	}
 	return (0);
