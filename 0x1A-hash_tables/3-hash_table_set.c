8f19967d6fa42c550cc92a9e74b636a37977fd6f
evie-8 <neouma29@gmail.com>
2023-06-09 17:12:57 +0300

    corrections

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL)
 		return (0);
-	if (strlen(key) == 0)
+	if (strlen(key) == 0 || key == NULL || value == NULL)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
