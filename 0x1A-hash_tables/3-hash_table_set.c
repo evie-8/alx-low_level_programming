b7098de0874448e28be32a7a69c5d15b494b96c7
evie-8 <neouma29@gmail.com>
2023-06-09 17:28:02 +0300

    corrections

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,21 +13,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL)
 		return (0);
-	if (strlen(key) == 0 || key == NULL || value == NULL)
+	if (strlen(key) == 0)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = 0; ht->array[i]; i++)
+	{
+		if (strcmp(key, ht->array[i]->key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+		}
+		return (1);
+	}
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
-	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
-	{
-		if (strcmp(key, ht->array[i]->key) == 0)
-			ht->array[i]->value = strdup(value);
-		return (1);
-	}
 	if (ht->array[index] == NULL)
 		ht->array[index] = new;
 	else
